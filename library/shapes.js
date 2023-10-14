563e4647bfe3f9237bfe37e7ec3218cce068d0f5
Cimarron <cimarron13@mac.com>
2023-10-14 10:30:41 -0500

    added shape photos

--- library/shapes.js
+++ library/shapes.js
@@ -21,7 +21,7 @@ class Circle extends Shape{
 
 class Square extends Shape{
     render(){
-        return `<rect x="50" y="83" width="200" height="100" fill="${this.shapeColor}"/>`;
+        return `<rect x="50" y="66" width="200" height="100" fill="${this.shapeColor}"/>`;
     }
 }
 
